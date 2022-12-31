10d9a88fc695e721566e64dd778c377f5b82a3a3
Ömer Faruk Karadeniz <krdnzomer@gmail.com>
2022-12-31 22:34:42 +0300

    init
    Signed-off-by: Ömer Faruk Karadeniz <krdnzomer@gmail.com>

---
+++ src/main/java/com/farukkaradeniz/log4jexperiments/Log4jExperimentsApplication.java
@@ -0,0 +1,13 @@
+package com.farukkaradeniz.log4jexperiments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Log4jExperimentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Log4jExperimentsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/farukkaradeniz/log4jexperiments/Log4jExperimentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farukkaradeniz.log4jexperiments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Log4jExperimentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
